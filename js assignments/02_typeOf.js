0fcf2a3ba54121ee449339bbb628d22a6a77c182
renukamali688 <renumali688@gmail.com>
2023-07-31 17:56:22 +0530

    Datatype of Before and After Initialization of variable
    Datatype of Before and After Initialization of variable

---
+++ js assignments/02_typeOf.js
@@ -0,0 +1,28 @@
+var bankName;
+console.log("Before Initialization: ", typeof bankName);
+bankName = " SBI Bank"; 
+console.log("After Initialization: ", bankName);
+console.log("Type of:", typeof bankName);
+
+console.log("---------------******------------------");
+var marks='90%';
+console.log("After Declaration and initialiatoon in one line:",marks);
+console.log("Type of: ", typeof marks);
+
+console.log("---------------******------------------");
+var isWorking;
+console.log("Before initialization:",typeof isWorking);
+isWorking=true;
+console.log("After initialization:", isWorking);
+console.log("Type of:", typeof isWorking);
+
+console.log("---------------******------------------");
+var totalCount="Hundred and Seven";
+console.log("After initialization:", totalCount);
+console.log("Type of:", typeof totalCount);
+totalCount=107;
+console.log("After change the value:", totalCount);
+console.log("Type of:", typeof totalCount);
+
+
+
