fba0685b73e8c9f7c3752b0fd3115e1c0996a33d
renukamali688 <renumali688@gmail.com>
2023-07-31 17:52:21 +0530

    Declaration and Initialization of variables
    Declaration and Initialization of variables

---
+++ js assignments/01_variableAssign.js
@@ -0,0 +1,37 @@
+var myFullName;
+console.log("Before Initialization:",  myFullName);
+
+myFullName="Renuka Malangner";
+console.log("After Initialization:",  myFullName);
+
+myFullName="Renuka Dnyaneshwar Malangner";
+console.log("After Updation:",  myFullName);
+
+console.log("----------------------------------------");
+
+console.log("Initialization and Declaration at a time: ")
+var myLove = "I love only JS"
+console.log("My Love:",  myLove);
+
+console.log("----------------------------------------");
+
+var firstName="Renuka";
+console.log("First Name:",  firstName);
+
+var lastName="Malangner";
+console.log("Last Name:",  lastName);
+
+var age = 23;
+console.log("Age:",  age);
+
+var address ="Pune";
+console.log("Address:",  address);
+
+var pinCode=411033;
+console.log("Pincode:",  pinCode);
+
+var gender="Female";
+console.log("Gender:",  gender);
+
+var is_married=false;
+console.log("is Married:", is_married);
